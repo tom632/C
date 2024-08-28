42229cdf7453d8a4173df85e49804be898a5f682
tom632 <thomas632@gmail.com>
2024-08-28 06:26:30 +0100

    Add files via upload

---
+++ songs/6.sql
@@ -0,0 +1,1 @@
+SELECT songs.name FROM songs JOIN artists on artists.id = songs.artist_id WHERE artists.name = 'Post Malone';
---
+++ songs/7.sql
@@ -0,0 +1,1 @@
+SELECT AVG(energy) FROM songs JOIN artists on artists.id = songs.artist_id WHERE artists.name = 'Drake';
